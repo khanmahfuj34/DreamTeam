24fd2dbf425141b15d2f35f624c0bf0fe0fdc9b2
khanmahfuj34 <mkmahfujkhanms3@gmail.com>
2026-03-07 04:09:32 +0600

    add project files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import navImg from "./assets/logo.png"
+import dollerImg from "./assets/dollar 1.png"
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="navbar max-w-[1200px] mx-auto">
+  <div className="flex-1">
+    <a className="text-xl"><img className='w-[60px],h-[60px]' src={navImg} alt="" /></a>
+  </div>
+  <div className="flex items-center">
+    <span className='mr-1 font-bold'>600000000</span>
+    <span className='mr-1 font-bold'> Coin</span>
+    <img src={dollerImg} alt="" />
+  </div>
+</div>
+    </>
+  )
+}
+
+export default App
